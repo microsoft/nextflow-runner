c2f710135c617abe47a75073aa757dc0cfca39a6
Sameer Doshi <sadoshi@microsoft.com>
2022-02-15 17:15:10 -0600

    Added Blazor Client App + Service + Pipeline rendering classes

---
+++ NextflowRunnerClient/Pages/PipelineList.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using NextflowRunnerClient.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NextflowRunnerClient.Pages
+{
+    public partial class PipelineList
+    {
+        [Inject]
+        protected NextflowAPI NfAPI { get; set; }
+
+        protected List<Pipeline> Pipelines { get; set; } = new List<Pipeline>();
+        protected string Errors { get; set; } = "";
+
+        protected async override Task OnInitializedAsync()
+        {
+            try
+            {
+                Pipelines = (List<Pipeline>)await NfAPI.GetPipelinesAsync();
+            }catch (ApiException ae)
+            {
+                Errors = ae.Message;
+            }
+            
+          
+        }
+    }
+}
