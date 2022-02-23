01984c5102933966b718cfff202f827f5fd52598
mjschanne <mjschanne@microsoft.com>
2022-02-23 16:00:26 -0500

    Clear up formatting

--- NextflowRunnerClient/Pages/ExecutePipeline.razor.cs
+++ NextflowRunnerClient/Pages/ExecutePipeline.razor.cs
@@ -12,22 +12,25 @@ public partial class ExecutePipeline
     [Parameter]
     public int Id { get; set; } = 0;
 
+    protected string RunNameUqEnd { get; set; } = Guid.NewGuid().ToString()[..6];
+    protected Pipeline Pipeline { get; set; } = new ();
     protected ICollection<ViewParam> Params { get; set; } = new List<ViewParam>();
-    protected ExecutionRequest ExecutionRequest { get; set; } = new() { RunName = Guid.NewGuid().ToString()[..6] };
+    protected ExecutionRequest ExecutionRequest { get; set; } = new() { RunName = string.Empty };
     protected bool SUBMITTED { get; set; } = false;
     protected bool VALID { get; set; } = false;
 
     protected async override Task OnInitializedAsync()
     {
-        var apiParams = await NfAPI.GetPipelineParamsAsync(Id);
+        Pipeline = await NfAPI.GetPipelineAsync(Id);
 
-        Params = apiParams.Select(p => new ViewParam(p)).ToList();
+        Params = Pipeline.PipelineParams.Select(p => new ViewParam(p)).ToList();
     }
 
     public async void ExecuteJob()
     {
         SUBMITTED = true;
 
+        ExecutionRequest.RunName += RunNameUqEnd;
         ExecutionRequest.Parameters = Params
             .ToDictionary(p => p.PipelineParamId.ToString(), p => p.Value);
 
