0ce79be5eb647c66cb16d884aae2e3a13685a1a3
mjschanne <mjschanne@microsoft.com>
2022-02-23 14:21:00 -0500

    Show local timezone

--- NextflowRunnerClient/Pages/ExecutePipeline.razor.cs
+++ NextflowRunnerClient/Pages/ExecutePipeline.razor.cs
@@ -13,7 +13,7 @@ public partial class ExecutePipeline
     public int Id { get; set; } = 0;
 
     protected ICollection<ViewParam> Params { get; set; } = new List<ViewParam>();
-    protected ExecutionRequest ExecutionRequest { get; set; } = new() { RunName = Guid.NewGuid().ToString().Substring(0,6) };
+    protected ExecutionRequest ExecutionRequest { get; set; } = new() { RunName = Guid.NewGuid().ToString()[..6] };
     protected bool SUBMITTED { get; set; } = false;
     protected bool VALID { get; set; } = false;
 
@@ -31,13 +31,6 @@ public partial class ExecutePipeline
         ExecutionRequest.Parameters = Params
             .ToDictionary(p => p.PipelineParamId.ToString(), p => p.Value);
 
-        // todo: revisit for updated api spec
-        try
-        {
-            await NfAPI.ExecutePipelineAsync(Id, ExecutionRequest);
-        } catch (Exception ex)
-        {
-            throw ex;
-        }
+        await NfAPI.ExecutePipelineAsync(Id, ExecutionRequest);
     }
 }
