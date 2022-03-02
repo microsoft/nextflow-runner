02475cfe2230b590173852ec9ffafec121e9be30
mjschanne <mjschanne@microsoft.com>
2022-03-02 12:24:34 -0500

    Add check for if input requires file input

--- NextflowRunnerClient/Components/RunListComponent.razor.cs
+++ NextflowRunnerClient/Components/RunListComponent.razor.cs
@@ -1,16 +1,9 @@
 ﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Rendering;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
+namespace NextflowRunnerClient.Components;
 
-namespace NextflowRunnerClient.Components
+public partial class RunListComponent
 {
-    public partial class RunListComponent
-    {
-       [Parameter]
-       public List<Services.PipelineRun> RunList { get; set; }
-    }
+    [Parameter]
+    public List<Services.PipelineRun> RunList { get; set; }
 }
